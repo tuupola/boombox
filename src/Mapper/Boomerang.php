fcbe96dab92097dc02562278f7d66f6249771609
Mika Tuupola <tuupola@appelsiini.net>
2016-02-26 15:41:07 +0700

    Ignore missing values

--- src/Mapper/Boomerang.php
+++ src/Mapper/Boomerang.php
@@ -46,42 +46,56 @@ class Boomerang
     public function transform()
     {
         $input = $this->getInput();
+        $metrics = [];
 
-        return [
-            /* http://www.lognormal.com/boomerang/doc/api/RT.html */
-            "roundtrip.firstbyte" =>
-                new Timing($input["t_resp"]),
-            "roundtrip.lastbyte" =>
-                new Timing($input["t_page"]),
-            "roundtrip.loadtime" =>
-                new Timing($input["t_done"]),
+        /* http://www.lognormal.com/boomerang/doc/api/RT.html */
+        if (isset($input["t_resp"])) {
+            $metrics["roundtrip.firstbyte"] = new Timing($input["t_resp"]);
+        }
+        if (isset($input["t_page"])) {
+            $metrics["roundtrip.lastbyte"] = new Timing($input["t_page"]);
+        }
+        if (isset($input["t_done"])) {
+            $metrics["roundtrip.loadtime"] = new Timing($input["t_done"]);
+        }
+        if (isset($input["nt_unload_end"]) && isset($input["nt_unload_st"])) {
+            $metrics["navtiming.unload"] = new Timing($input["nt_unload_end"] - $input["nt_unload_st"]);
+        }
+        if (isset($input["nt_red_end"]) && isset($input["nt_red_st"])) {
+            $metrics["navtiming.redirect"] = new Timing($input["nt_red_end"] - $input["nt_red_st"]);
+        }
+        if (isset($input["nt_dns_st"]) && isset($input["nt_fet_st"])) {
+            $metrics["navtiming.cache"] = new Timing($input["nt_dns_st"] - $input["nt_fet_st"]);
+        }
+        if (isset($input["nt_dns_end"]) && isset($input["nt_dns_st"])) {
+            $metrics["navtiming.dns"] = new Timing($input["nt_dns_end"] - $input["nt_dns_st"]);
+        }
+        if (isset($input["nt_con_end"]) && isset($input["nt_con_st"])) {
+            $metrics["navtiming.tcp"] = new Timing($input["nt_con_end"] - $input["nt_con_st"]);
+        }
+        if (isset($input["nt_res_st"]) && isset($input["nt_req_st"])) {
+            $metrics["navtiming.request"] = new Timing($input["nt_res_st"] - $input["nt_req_st"]);
+        }
+        if (isset($input["nt_res_end"]) && isset($input["nt_res_st"])) {
+            $metrics["navtiming.response"] = new Timing($input["nt_res_end"] - $input["nt_res_st"]);
+        }
+        if (isset($input["nt_domcomp"]) && isset($input["nt_domloading"])) {
+            $metrics["navtiming.processing"] = new Timing($input["nt_domcomp"] - $input["nt_domloading"]);
+        }
+        if (isset($input["nt_load_end"]) && isset($input["nt_load_st"])) {
+            $metrics["navtiming.onload"] = new Timing($input["nt_load_end"] - $input["nt_load_st"]);
+        }
+        if (isset($input["nt_load_end"]) && isset($input["nt_nav_st"])) {
+            $metrics["navtiming.loadtime"] = new Timing($input["nt_load_end"] - $input["nt_nav_st"]);
+        }
 
-            "navtiming.unload" =>
-                new Timing($input["nt_unload_end"] - $input["nt_unload_st"]),
-            "navtiming.redirect" =>
-                new Timing($input["nt_red_end"] - $input["nt_red_st"]),
-            "navtiming.cache" =>
-                new Timing($input["nt_dns_st"] - $input["nt_fet_st"]),
-            "navtiming.dns" =>
-                new Timing($input["nt_dns_end"] - $input["nt_dns_st"]),
-            "navtiming.tcp" =>
-                new Timing($input["nt_con_end"] - $input["nt_con_st"]),
-            "navtiming.request" =>
-                new Timing($input["nt_res_st"] - $input["nt_req_st"]),
-            "navtiming.response" =>
-                new Timing($input["nt_res_end"] - $input["nt_res_st"]),
-            "navtiming.processing" =>
-                new Timing($input["nt_domcomp"] - $input["nt_domloading"]),
-            "navtiming.onload" =>
-                new Timing($input["nt_load_end"] - $input["nt_load_st"]),
-            "navtiming.loadtime" =>
-                new Timing($input["nt_load_end"] - $input["nt_nav_st"]),
+        if (isset($input["mem_total"])) {
+            $metrics["memory.total"] = new Gauge($input["mem_total"]);
+        }
+        if (isset($input["mem_used"])) {
+            $metrics["memory.used"] = new Gauge($input["mem_used"]);
+        }
 
-            /* parse_str() converts . to _ */
-            "memory.total" =>
-                new Gauge($input["mem_total"]),
-            "memory.used" =>
-                new Gauge($input["mem_used"]),
-        ];
+        return $metrics;
     }
 }
